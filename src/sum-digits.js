c26eeca3fb6d798601c960837c06f2978c9ea2f8
Tatiana Shipulina <grafit.1@yandex.ru>
2022-10-17 16:13:10 +0300

    feat: add resolution Part 4

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let sum = 0;
+  let sum2 = 0;
+  let str = String(n);
+  const arr = str.split('');
+  arr.forEach((item) => {
+    sum += Number(item);
+  })
+  sum = String(sum);
+  if(sum.length > 1) {
+    const arr2 = sum.split('');
+    sum = Number(sum);
+    arr2.forEach((item) => {
+      sum2 = sum2 + Number(item)
+    })
+    return sum2;
+  }
+  return Number(sum);
 }
 
 module.exports = {
